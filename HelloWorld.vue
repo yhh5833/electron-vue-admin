d8ef112f5350bb56612eaee6dd1553ab0ed06c3b
yhh5833 <1295476388@qq.com>
2025-02-16 13:49:00 +0800

    Create HelloWorld.vue

---
+++ HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="100" />
+    <HelloWorld msg="Welcome to Vue Practice!" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
